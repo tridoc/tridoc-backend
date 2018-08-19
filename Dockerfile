c3c56b73e6677019761d3514acbc63b15cf06bb3
Reto Gmür <reto@factsmission.com>
2018-08-19 14:16:00 +0200

    Changed irectoty layout, using yarn in docker

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
-FROM node:8.11-onbuild
+FROM node:8.11-alpine
 EXPOSE 8000
-# ADD docker-cmd.sh /usr/local/
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+COPY . /usr/src/app
+RUN yarn install
 RUN chmod +x /usr/src/app/docker-cmd.sh
 CMD [ "/usr/src/app/docker-cmd.sh" ]
--- docker-cmd.sh
+++ docker-cmd.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 sleep 5
 echo 'Attempting to create Dataset "3DOC"'
 curl 'http://fuseki:3030/$/datasets' -H "Authorization: Basic $(echo -n admin:pw123 | base64)" \
     -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' --data 'dbName=3DOC&dbType=tdb'
 set -m
-npm start &
+yarn start &
 sleep 5
 echo 'Attempting to create Dataset "3DOC"'
 curl 'http://fuseki:3030/$/datasets' -H "Authorization: Basic $(echo -n admin:pw123 | base64)" \
