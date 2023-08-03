9d17a038e920a6e9186e5bb63f5768e305939748
nleanba <25827850+nleanba@users.noreply.github.com>
2023-08-03 18:31:58 +0200

    Removed deadlock
    Apparently, waiting for status before waiting for output will wait forever

--- src/helpers/pdfprocessor.ts
+++ src/helpers/pdfprocessor.ts
@@ -2,7 +2,8 @@ const decoder = new TextDecoder("utf-8");
 
 export async function getText(path: string) {
   const p = Deno.run({ cmd: ["pdftotext", path, "-"], stdout: "piped" });
+  const output = decoder.decode(await p.output());
   const { success, code } = await p.status();
-  if (!success) throw new Error("pdfsandwich failed with code " + code);
-  return decoder.decode(await p.output());
+  if (!success) throw new Error("pdftotext failed with code " + code);
+  return output;
 }
